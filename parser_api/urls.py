b7aac5b1bb636336f5eedc2d3878f91ccd45f06f
anastasia <petrichuk.nastya@yandex.ru>
2021-11-10 15:36:02 +0300

    оптимизированы импорты

--- parser_api/urls.py
+++ parser_api/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from .views import GetDataView, HelpView
+from django.urls import path
 
+from .views import GetDataView, HelpView
 
 urlpatterns = [
     path('', GetDataView.as_view(), name='get_data_from_page'),
