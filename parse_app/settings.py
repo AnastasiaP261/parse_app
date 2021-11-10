414cfe9c4b23c0daa4a3f0decb55c26e20728800
anastasia <petrichuk.nastya@yandex.ru>
2021-11-10 17:01:33 +0300

    исправлено под хост контейнера 0.0.0.0

--- parse_app/settings.py
+++ parse_app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-5uz9n(*&n6dqm+-h*9y__=$wnrs$cp=hjh6f)rg7)t49muk+(@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', ]
 
 # Application definition
 
